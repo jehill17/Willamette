46889d91468a4c93247f1674a1b43d235e3ffdd5
Joy Hill <jehill17@live.unc.edu>
2018-05-14 16:57:59 -0400

    cp influenced reservoirs

--- Willamette_outer.py
+++ Willamette_outer.py
@@ -118,7 +118,7 @@ CP = [ControlPoint(id) for id in range(1, len(cp_list)+1)]
 for cp in CP:
     id = cp.ID
     cp.name = cp_list[id-1]
-    cp.influencedReservoirs =str(controlPoints[id-1]["@reservoirs"])
+    cp.influencedReservoirs =np.asarray((controlPoints[id-1]["@reservoirs"]).split(','))
     cp.COMID=str(controlPoints[id-1]["@location"])
 
 
